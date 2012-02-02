fc1214510304569b685485e1076fdc538159de67
Seth Petry-Johnson <seth@petry-johnson.com>
2012-02-02 09:16:47 -0500

    Implemented Strength modifier during attacks

---
+++ Evercraft_model/AttackResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Evercraft_model {
+
+	public class AttackResult {
+		public int AttackRoll { get; set; }
+		public bool Success { get; set; }
+		public int Damage { get; set; }
+	}
+}
